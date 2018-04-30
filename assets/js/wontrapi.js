66582792c45206a1a8dfa29982e5f460c7712ad6
WPGuru4u <wpguru4u@gmail.com>
2018-04-30 12:47:08 -0400

    correct prefix

--- assets/js/wontrapi.js
+++ assets/js/wontrapi.js
@@ -1,30 +1,28 @@
-window.webdev_ajax = ( function( window, document, $ ){
+window.wontrapi_ajax = ( function( window, document, $ ){
 	var app = {};
 
 	app.cache = function(){
-		app.$ajax_form = $( '.ajax_tut_form' );
+		app.$ajax_form = $( '.wontrapi_bnb' );
 	};
 
 	app.init = function(){
 		app.cache();
-		app.$ajax_form.on( 'submit', app.form_handler );
-
+		app.$ajax_form.on( 'click', app.form_handler );
 	};
 
-	app.post_ajax = function( serial_data ){
+	app.post_ajax = function( data ){
 		var post_data = { 
-			action     : 'webdev',
-			nonce      : webdev.nonce,
-			serialized : serial_data,
+			action       : 'wontrapi_bnb',
+			nonce        : wontrapi.nonce,
+			wontrapi_bnb : data,
 		};
-
-		$.post( webdev.ajax_url, post_data, app.ajax_response, 'json' )
+		$.post( wontrapi.ajax_url, post_data, app.ajax_response, 'json' )
 	};
 
 	app.ajax_response = function( response_data ){
 		if( response_data.success ){
-			webdev.nonce = response_data.data.nonce;
-			alert( response_data.data.script_response );
+			wontrapi.nonce = response_data.data.nonce;
+			top.location.replace(response_data.data.url);
 		} else {
 			alert( 'ERROR' );
 		}
@@ -32,8 +30,10 @@ window.webdev_ajax = ( function( window, document, $ ){
 
 	app.form_handler = function( evt ){
 		evt.preventDefault();
-		var serialized_data = app.$ajax_form.serialize();
-		app.post_ajax( serialized_data );
+		var data = jQuery(this).data();
+		// disable to avoid duplicate transactions
+		$(".wontrapi_bnb").attr("disabled", true);
+		app.post_ajax( data );
 	};
 
 	$(document).ready( app.init );
