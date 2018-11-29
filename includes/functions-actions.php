d803795fa1f3728dd4dcc5845200f1fb68c72fe5
WPGuru4u <wpguru4u@gmail.com>
2018-11-29 17:10:09 -0500

    Remove BNB to own plugin

--- includes/functions-actions.php
+++ includes/functions-actions.php
@@ -1,20 +1,4 @@
 <?php
 
 
-function wontrapi_click_and_buy( $atts = array() ) {
 
-	$invoice_template = (isset($atts['invoice_template'])?$atts['invoice_template']:1);
-	$gateway_id = $atts['gateway_id'];
-	$offer = array(
-
-	);
-
-	$success_link = '';
-	
-
-	$contact_id = $_COOKIE["contact_id"];
-
-	WontrapiGo::transaction_process( $contact_id, $invoice_template = 1, $gateway_id, $offer );
-
-}
-add_shortcode( 'wontrapi_click_and_buy', 'wontrapi_click_and_buy' );
