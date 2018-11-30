103058e6c42d2c6513af58eb5bbfab6b636595d5
WPGuru4u <wpguru4u@gmail.com>
2018-11-30 03:41:24 -0500

    Just one script; move to actions.php

--- includes/actions.php
+++ includes/actions.php
@@ -11,11 +11,21 @@ function wontrapi_listen() {
 	if ( isset( $p["$ping_key"] ) ) {
 		if ( $p["$ping_key"] == $ping_val ) {
 			if ( isset( $p['wontrapi_action'] ) ) {
+				// fire a specific action based on the event
 				do_action( "wontrapi_post_action_{$p['wontrapi_action']}", $p );
-			} else {
-				do_action( 'wontrapi_post_action', $p );
+		//	} else {
+				// fire a general action for all events
+		//		do_action( 'wontrapi_post_action', $p );
 			}
 		}
 	}
 }
 add_action( 'init', 'wontrapi_listen' );
+
+function wontrapi_tracking_script() {
+	$data = get_option( 'wontrapi_options' );
+	if ( !empty( $data['tracking'] ) ) {
+		echo $data['tracking'];
+	}
+}
+add_action( 'wp_footer', 'wontrapi_tracking_script' );
--- includes/scripts.php
+++ includes/scripts.php
@@ -1,9 +1,3 @@
 <?php 
 
-function wontrapi_tracking_script() {
-	$data = get_option( 'wontrapi_options' );
-	if ( !empty( $data['tracking'] ) )
-		echo $data['tracking'];
 
-}
-add_action( 'wp_footer', 'wontrapi_tracking_script' );
