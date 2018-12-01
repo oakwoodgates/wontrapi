ae386e905cfa3a32edc5c0f1ebef6cd1bb96b8ca
WPGuru4u <wpguru4u@gmail.com>
2018-12-01 03:39:50 -0500

    Move actions to class

---
+++ includes/class-actions.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * Wontrapi Actions.
+ *
+ * @since   0.4.0
+ * @package Wontrapi
+ */
+
+/**
+ * Wontrapi Actions.
+ *
+ * @since 0.4.0
+ */
+class Wontrapi_Actions {
+	/**
+	 * Parent plugin class.
+	 *
+	 * @since 0.4.0
+	 *
+	 * @var   Wontrapi
+	 */
+	protected $plugin = null;
+
+	/**
+	 * Constructor.
+	 *
+	 * @since  0.4.0
+	 *
+	 * @param  Wontrapi $plugin Main plugin object.
+	 */
+	public function __construct( $plugin ) {
+		$this->plugin = $plugin;
+		$this->hooks();
+	}
+
+	/**
+	 * Initiate our hooks.
+	 *
+	 * @since  0.4.0
+	 */
+	public function hooks() {
+		add_action( 'init', 		array( $this, 'listen' ) );
+		add_action( 'wp_footer', 	array( $this, 'tracking_script' ) );
+	}
+
+	function listen() {
+		$data = get_option( 'wontrapi_options' );
+		// if ( empty( $data['ping_key'] ) || empty( $data['ping_value'] )	) {
+		if ( empty( $data['ping_value'] ) ) {
+			return;
+		}
+		$ping_key = ( $data['ping_key'] ) ? $data['ping_key'] : 'wontrapi_key';
+		$ping_val = $data['ping_value'];
+		if ( isset( $_POST["$ping_key"] ) ) {
+			if ( $_POST["$ping_key"] == $ping_val ) {
+				if ( isset( $_POST['wontrapi_action'] ) ) {
+					// fire a specific action based on the event
+					do_action( "wontrapi_post_action_{$_POST['wontrapi_action']}", $_POST );
+				}
+			}
+		}
+	}
+
+	function tracking_script() {
+		$data = get_option( 'wontrapi_options' );
+		if ( !empty( $data['tracking'] ) ) {
+			echo $data['tracking'];
+		}
+	}
+
+}
